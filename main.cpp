e56e62b0558f18428faeaea717b55626db8dfcfa
MrHeadQ <2460753095@qq.com>
2020-07-09 14:09:42 +0800

    test

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+void sort(int *arr,int size)
+{}
+
+int main()
+{
+	int arr[] = { 1,2,3,4,2,3,2,3 };
+	int size = sizeof(arr / arr[0]);
+	sort(arr, size);
+	return 0;
+}
